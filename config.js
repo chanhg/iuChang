8b182e8403ff7a85aafad83245b888c93d1a402c
chanhg <92108331+chanhg@users.noreply.github.com>
2021-10-07 23:29:00 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,19 @@
+const CONFIG = {
+    introTitle: 'Chang à!',
+    introDesc: `Trái đất vốn lạ thường
+    Mà sao em cứ đi nhầm đường
+    Lạc vào tim anh lẻ loi
+    Đằng sau chữ yêu đây là thương`,
+    btnIntro: 'hihi',
+    title: 'Phải chăng anh đã say ngay từ lúc thấy nụ cười ấy',
+    desc: 'Chúng ta lại yêu nhau nhé',
+    btnYes: 'OK luôn <33',
+    btnNo: 'Không :3',
+    question:'Trên thế giới hơn 7 tỉ người mà sao Thanh lại cứ yêu Chang  <3',
+    btnReply: 'Iu <3',
+    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
+    mess: 'Yêu em nhiều nhiều 😘😘',
+    messDesc: 'Miu Miu',
+    btnAccept: 'Okiiiii lun <3',
+    messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
+}
