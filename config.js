bd4984efee572b57c5e6c287e641402ba468ee67
chanhg <92108331+chanhg@users.noreply.github.com>
2021-10-16 22:59:31 +0700

    Update config.js

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ const CONFIG = {
     Mà sao em cứ đi nhầm đường -
     Lạc vào tim anh lẻ loi -
     Đằng sau chữ yêu đây là thương`,
-    btnIntro: 'hihi',
+    btnIntro: 'Tiếp >>>',
     title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên',
     desc: 'Chúng ta lại yêu nhau nhé!!!',
     btnYes: 'OK luôn <33',
